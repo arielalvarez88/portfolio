3320c2bacee2f04298ebcc8b58ffac4b963a715c
Ariel Alvarez <arielalvarez88@gmail.com>
2017-01-16 08:23:11 -0400

    fixing content smaller than screen problem

--- js/react/sections/InfoSection.jsx
+++ js/react/sections/InfoSection.jsx
@@ -19,7 +19,12 @@ export class InfoSection extends React.Component {
                     <div className="col-xs-8 col-sm-offset-2 appear animation-element">
                         <table className="info-table">
                             <tr>
-                                <td className="vertical-text">Passion for software.</td>
+                                <td >
+                                  <div id="vertical-text" className="vertical-text">
+                                    Passion for software.
+                                  </div>
+
+                                </td>
 
                                 <td>Full Name: Ariel Alvarez
                                     <br/>
@@ -41,6 +46,26 @@ export class InfoSection extends React.Component {
                                 </td>
                             </tr>
                         </table>
+                        {/*<div className="vertical-text col-xs-1" style={{height:550}}>Passion for software</div>
+                        <div className="col-xs-11" style={{height:550}}>
+                            Full Name: Ariel Alvarez
+                              <br/>
+                              <br/>
+                              Email: arielalvarez88@gmail.com
+                              <br/>
+                              <br/>
+                              Phone: 1-829-383-4141
+                              <br/>
+                              <br/>
+                              LinkedIn: https://do.linkedin.com/in/ariel-alvarez-86b11a88
+                              <br/>
+                              <br/>
+                              GitHub: https://github.com/arielalvarez88
+                              <br/>
+                              <br/>
+                              Portfolio: You are seeing it.
+                        </div>*/}
+
                     </div>
                 </div>
 
