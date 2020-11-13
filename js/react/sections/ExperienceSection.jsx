e1f8b5c85b462382a3c6b7ac1d3aa8530e2a60cd
Ariel Alvarez <arielalvarez88@gmail.com>
2020-11-13 07:16:34 -0400

    adding dev script to recompile when I change something

--- js/react/sections/ExperienceSection.jsx
+++ js/react/sections/ExperienceSection.jsx
@@ -15,9 +15,8 @@ export class ExperienceSection extends React.Component {
                     </div>
 
                     <div className="row">
-                        <div className="col-md-4 bullet-box-container animation-element appear-from-left">
                             <BulletBox
-                                className="separated"
+                                className="separated col-md-4 bullet-box-container animation-element appear-from-left"
                                 headerCls="red"
                                 title="Okkralabs, San Jose, California"
                                 subText="Software Engineer, November 2013 – Present"
@@ -27,10 +26,20 @@ export class ExperienceSection extends React.Component {
                                 ]}
                             />
 
-                        </div>
+                            <BulletBox
+                                className="separated col-md-4 bullet-box-container animation-element appear-from-left"
+                                headerCls="red"
+                                title="Okkralabs, San Jose, California"
+                                subText="Software Engineer, November 2013 – Present"
+                                bullets={[
+                                    {text: "Developed web application with latest web technologies (JS, IndexedDB, WebSockets, Rabbitmq)"},
+                                    {text: "Increased quality and client satisfaction by introducing Scrum methodology to team. Worked as Scrum Master and implemented metrics gathering and reporting that didn’t exist before."}
+                                ]}
+                            />
 
-                        <div className="col-md-4 bullet-box-container  animation-element appear">
-                            <BulletBox className="separated" headerCls="red" title="Claro, Santo Domingo, Dominican Republic" subText="System Engineer Level 3, July 2012 – November 2013"
+
+
+                            <BulletBox className="separated col-md-4 bullet-box-container  animation-element appear" headerCls="red" title="Claro, Santo Domingo, Dominican Republic" subText="System Engineer Level 3, July 2012 – November 2013"
                                        bullets={[
                                            {text: "Developed web applications as a full stack developer for inventory management."},
                                            {text: "Developed web services for service activation, client charging and info request for different cell phone services."},
@@ -39,25 +48,21 @@ export class ExperienceSection extends React.Component {
                                        ]}
                             />
 
-                        </div>
 
-                        <div className="col-md-4 bullet-box-container  animation-element appear-from-right">
-                            <BulletBox className="separated" headerCls="red" title="5050MKT, Santiago, Dominican Republic" subText="Full Stack Developer, April 2011 – March2012"
+                            <BulletBox className="separated col-md-4 bullet-box-container  animation-element appear-from-right" headerCls="red" title="5050MKT, Santiago, Dominican Republic" subText="Full Stack Developer, April 2011 – March2012"
                                        bullets={[
                                            {text: "Developed web applications as a full stack developer using CMS using PHP, Mysql, Drupal and JQuery."},
                                        ]}
                             />
-                        </div>
                     </div>
 
                     <div className="row">
-                        <div className="col-md-4 bullet-box-container animation-element appear-from-left">
-                            <BulletBox className="separated" headerCls="red" title="Intellisys, Santiago, Dominican Republic" subText="Scrum Team Member, November 2010– April 2011"
+
+                            <BulletBox className="separated col-md-4 bullet-box-container animation-element appear-from-left" headerCls="red" title="Intellisys, Santiago, Dominican Republic" subText="Scrum Team Member, November 2010– April 2011"
                                        bullets={[
                                            {text: "Develop web application noodle.org as a full stack developer using PHP, Mysql, Drupal, JQuery, Ruby Migrations, TeamCity."},
                                        ]}
                             />
-                        </div>
                     </div>
 
                 </div>
